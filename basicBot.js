c06da2e0a7edbfa5b6510f35626986de40a08124
tdlive aw'sum <iam@tdlive.me>
2014-08-18 21:44:32 -0400

    Blasted semicolon >:(

--- basicBot.js
+++ basicBot.js
@@ -5,7 +5,7 @@ function init(twimod){
 
   eh.registerEvent("loggedOn", function(){
     bot.setPersonaState(twimod.steam.EPersonaState.Online);
-  })
+  });
 
   eh.registerCommand("about", function(msg){
     msg.reply(twimod.bot.users[twimod.bot.steamID].playerName + "\n(c) 2013-2014 No More Sugar, tdlive aw'sum & contributors.\nPowered by IWriteLists " + msg.twimod.version + ". http://github.com/NoMoreSugar/iwritelists");
